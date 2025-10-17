6fa4898f12f3efb9b9cb2a28bd521d8736f12225
NoVip777 <isaqueodcunha@gmail.com>
2025-10-17 16:14:04 -0300

    update Bird.js

--- Bird.js
+++ Bird.js
@@ -1,1 +1,32 @@
-
+class Bird{
+  constructor(ctx){
+    this.ctx=ctx
+    this.gravidade=3
+    this.pulo=5
+    this.caindo=false
+    this.largura=25
+    this.altura=20
+    this.x=200
+    this.y=(this.ctx.canvas.height/2)
+  }
+  
+  iniciar(){
+    this.caindo=true
+    this.y=(this.ctx.canvas.height/2)
+  }
+  pular(){
+    this.y-=this.pulo
+  }
+  
+  gerenciar(){
+    if(this.caindo){
+      this.y+=this.gravidade
+    }
+  }
+  
+  desenhar(){
+    this.gerenciar()
+    this.ctx.fillStyle='ff0'
+    this.ctx.fillRect(this.x, this.y, this.largura, this.altura)
+  }
+}
