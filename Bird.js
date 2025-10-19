91a7701c81290c5cfe241850e43ca548bcec1961
NoVip777 <isaqueodcunha@gmail.com>
2025-10-19 11:15:21 -0300

    delay de pulo Bird.js
    adicionado delay de pulo e comentarios

--- Bird.js
+++ Bird.js
@@ -1,31 +1,45 @@
 class Bird{
   constructor(ctx){
+    // algumas variaveis
     this.ctx=ctx
     this.gravidade=0.3
     this.velY=0
-    this.pulo=-17
+    this.pulo=-12
     this.caindo=false
     this.largura=25
     this.altura=20
     this.x=200
     this.y=(this.ctx.canvas.height/2)
+    
+    // delay de pulo
+    this.delay=0
+    this.prontoPular=20
   }
   
+  // inicia o joguinho
   iniciar(){
     this.caindo=true
     this.y=(this.ctx.canvas.height/2)
   }
-  pular(){
-    this.velY+=this.pulo
-    }
   
+  // faz pular com delay
+  pular(){
+    if(this.delay >= this.prontoPular){
+      this.velY+=this.pulo
+      this.delay=0
+    }
+  }
+  
+  // faz o game atualiazar a gravidade, pulo, etc...
   gerenciar(){
     if(this.caindo){
       this.velY += this.gravidade
       this.y+=this.velY
+      this.delay++
     }
   }
   
+  // desenha/atualiza o gerenciar e o quadrado do bird
   desenhar(){
     this.gerenciar()
     this.ctx.fillStyle='#ff0'
@@ -33,10 +47,12 @@ class Bird{
   }
 }
 
+// fucao que chama o iniciar
 function iniciar(){
   bird.iniciar()
 }
 
+// fucao que chama o pular
 function pular(){
   bird.pular()
 }
