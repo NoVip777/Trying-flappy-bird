fe2b907dd8568849a78d73dd70760badf63c4f7e
NoVip777 <isaqueodcunha@gmail.com>
2025-10-27 14:22:20 -0300

    update Pipes.js

--- Pipes.js
+++ Pipes.js
@@ -2,15 +2,24 @@ class Pipes{
   constructor(ctx, bird){
     this.ctx = ctx
     this.bird = bird
-    this.ax = 480
+    this.yAleatorio = Math.floor(Math.random() * (480-20*1)) + 20;
+    this.ax = 500
     this.ay = this.yAleatorio
     this.largura = 30
     this.altura = 500
-    this.yAleatorio = Math.floor(Math.random() * (480-20*1)) + 20;
+    this.velocidade = 3
   }
   
   gerenciar(){
-    
+    if(this.bird.caindo){
+      this.ax -= this.velocidade
+      if(this.ax < 0-this.largura){
+        this.ay = this.yAleatorio
+        this.ax = 500
+      }
+    }else{
+      this.ax -= 0
+    }
   }
   
   desenhar(){
