d6b5670655abf23bdad5925fed6bb8f1f0e041c0
NoVip777 <isaqueodcunha@gmail.com>
2025-10-27 14:12:30 -0300

    update Pipes.js

--- Pipes.js
+++ Pipes.js
@@ -1,7 +1,12 @@
 class Pipes{
-  constructor(ctx){
+  constructor(ctx, bird){
     this.ctx = ctx
-    
+    this.bird = bird
+    this.ax = 480
+    this.ay = this.yAleatorio
+    this.largura = 30
+    this.altura = 500
+    this.yAleatorio = Math.floor(Math.random() * (480-20*1)) + 20;
   }
   
   gerenciar(){
@@ -10,6 +15,7 @@ class Pipes{
   
   desenhar(){
     this.gerenciar()
-    
+    this.ctx.fillStyle = '#0f0'
+    this.ctx.fillRect(this.ax, this.ay, this.largura, this.altura)
   }
 }
